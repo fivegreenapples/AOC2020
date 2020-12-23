8229a2ddf125154d737951296aa70526f3b76c86
Ben Holland <fivegreenapples@users.noreply.github.com>
2020-12-23 21:04:35 +0000

    Complete refactor and optimise day 23.

--- days/day23.go
+++ days/day23.go
@@ -6,129 +6,108 @@ import (
 )
 
 func (r *Runner) Day23Part1(in string) string {
-	oneCup := r.d23ProcessInputAndPlay(in, 9, 100)
-	return oneCup.value()
+	lookup := r.d23ProcessInputAndPlay(in, 9, 100)
+	return r.d23GetValue(lookup)
 }
 
 func (r *Runner) Day23Part2(in string) string {
-	oneCup := r.d23ProcessInputAndPlay(in, 1000000, 10000000)
-	return strconv.Itoa(oneCup.next.val * oneCup.next.next.val)
+	lookup := r.d23ProcessInputAndPlay(in, 1000000, 10000000)
+	return strconv.Itoa(lookup[1] * lookup[lookup[1]])
 }
 
-func (r *Runner) d23ProcessInputAndPlay(in string, numCups int, numRounds int) *d23Cup {
+func (r *Runner) d23ProcessInputAndPlay(in string, numCups int, numRounds int) []int {
 
-	var firstCup d23Cup
-	lookup := map[int]*d23Cup{}
+	lookup := make([]int, numCups+1)
 
-	firstCup = d23Cup{
-		val: int(in[0] - '0'),
-	}
-	lookup[firstCup.val] = &firstCup
-
-	thisCup := &firstCup
+	firstVal := int(in[0] - '0')
+	prevVal := firstVal
 	for _, rn := range in[1:] {
-		thisCup.next = &d23Cup{
-			val: int(rn - '0'),
-		}
-		lookup[thisCup.next.val] = thisCup.next
-		thisCup = thisCup.next
+		lookup[prevVal] = int(rn - '0')
+		prevVal = int(rn - '0')
 	}
 	for i := len(in) + 1; i <= numCups; i++ {
-		thisCup.next = &d23Cup{
-			val: i,
-		}
-		lookup[thisCup.next.val] = thisCup.next
-		thisCup = thisCup.next
+		lookup[prevVal] = i
+		prevVal = i
 	}
-	thisCup.next = &firstCup
+	lookup[prevVal] = firstVal
 
-	thisCup = &firstCup
+	var destinationVal int
+	val := firstVal
 	for i := 0; i < numRounds; i++ {
-		thisCup.playRound(lookup, numCups)
-		thisCup = thisCup.next
-	}
 
-	return lookup[1]
-}
+		/*
+			The crab picks up the three cups that are immediately clockwise of the
+			current cup. They are removed from the circle; cup spacing is adjusted as
+			necessary to maintain the circle.
 
-type d23Cup struct {
-	val  int
-	next *d23Cup
-}
+			The crab selects a destination cup: the cup with a label equal to the
+			current cup's label minus one. If this would select one of the cups that was
+			just picked up, the crab will keep subtracting one until it finds a cup that
+			wasn't just picked up. If at any point in this process the value goes below
+			the lowest value on any cup's label, it wraps around to the highest value on
+			any cup's label instead.
 
-func (c *d23Cup) playRound(lookup map[int]*d23Cup, max int) {
+			The crab places the cups it just picked up so that they are immediately
+			clockwise of the destination cup. They keep the same order as when they were
+			picked up.
 
-	/*
-		The crab picks up the three cups that are immediately clockwise of the
-		current cup. They are removed from the circle; cup spacing is adjusted as
-		necessary to maintain the circle.
+			The crab selects a new current cup: the cup which is immediately clockwise
+			of the current cup.
+		*/
 
-		The crab selects a destination cup: the cup with a label equal to the
-		current cup's label minus one. If this would select one of the cups that was
-		just picked up, the crab will keep subtracting one until it finds a cup that
-		wasn't just picked up. If at any point in this process the value goes below
-		the lowest value on any cup's label, it wraps around to the highest value on
-		any cup's label instead.
-
-		The crab places the cups it just picked up so that they are immediately
-		clockwise of the destination cup. They keep the same order as when they were
-		picked up.
-
-		The crab selects a new current cup: the cup which is immediately clockwise
-		of the current cup.
-	*/
-
-	// find destination
-	destinationVal := c.val - 1
-	for destinationVal == 0 ||
-		destinationVal == c.next.val ||
-		destinationVal == c.next.next.val ||
-		destinationVal == c.next.next.next.val {
-		if destinationVal == 0 {
-			destinationVal = max
-		} else {
-			destinationVal--
+		aVal := lookup[val]
+		bVal := lookup[aVal]
+		cVal := lookup[bVal]
+		dVal := lookup[cVal]
+		// find destination
+		destinationVal = val - 1
+		for destinationVal == 0 ||
+			destinationVal == aVal ||
+			destinationVal == bVal ||
+			destinationVal == cVal {
+			if destinationVal == 0 {
+				destinationVal = numCups
+			} else {
+				destinationVal--
+			}
 		}
+
+		// connect current to one after the group of three
+		lookup[val] = dVal
+
+		// insert group of three
+		lookup[cVal] = lookup[destinationVal]
+		lookup[destinationVal] = aVal
+
+		val = dVal
 	}
-	destination := lookup[destinationVal]
 
-	// grab reference to head and tail of group of three
-	firstOfThree := c.next
-	lastOfThree := c.next.next.next
-
-	// connect current to one after the group of three
-	c.next = c.next.next.next.next
-
-	// insert group of three
-	lastOfThree.next = destination.next
-	destination.next = firstOfThree
-
+	return lookup
 }
 
-func (c d23Cup) String() string {
+// }
 
-	curVal := c.val
-	str := fmt.Sprint(c.val)
-	c = *c.next
-	for c.val != curVal {
-		str += fmt.Sprint(c.val)
-		c = *c.next
-	}
+// func (c d23Cup) String() string {
 
-	return str
-}
+// curVal := c.val
+// str := fmt.Sprint(c.val)
+// c = *c.next
+// for c.val != curVal {
+// 	str += fmt.Sprint(c.val)
+// 	c = *c.next
+// }
 
-func (c d23Cup) value() string {
+// return str
+// }
 
-	for c.val != 1 {
-		c = *c.next
-	}
-	c = *c.next
+func (r *Runner) d23GetValue(lookup []int) string {
+
+	c := lookup[1]
 
 	str := ""
-	for c.val != 1 {
-		str += fmt.Sprint(c.val)
-		c = *c.next
+	for c != 1 {
+		str += fmt.Sprint(c)
+		c = lookup[c]
 	}
 
 	return str
